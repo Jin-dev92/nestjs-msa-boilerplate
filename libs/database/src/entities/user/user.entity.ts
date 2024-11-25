fb0278e4b30fa5b4610d62bf8e68f83e8477d512
Jin-devST <kpasd002@gmail.com>
2024-11-25 15:35:26 +0900

    feat. 데이터베이스 라이브러리 추가

--- libs/database/src/entities/user/user.entity.ts
+++ libs/database/src/entities/user/user.entity.ts
@@ -1,11 +1,21 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { CommonEntity } from './common.entity';
+import { CommonEntity } from '@libs/database/entities';
+import { UserRoleEnum } from '@libs/database/enum';
 
 @Entity()
 export class User extends CommonEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({ unique: true })
+  email: string;
+
   @Column()
-  name: string;
+  password: string;
+
+  @Column({ length: 100 })
+  username: string;
+
+  @Column({ type: 'enum', enum: UserRoleEnum, default: UserRoleEnum.USER })
+  role: UserRoleEnum;
 }
