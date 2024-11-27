362526447e8771bb88d20ae915214c42b299001c
Jin-devST <kpasd002@gmail.com>
2024-11-27 11:51:23 +0900

    feat. nestjs-msa-boilerplate app 제거 (필요없어서 제거)

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { ApiGatewayController } from './api-gateway.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { MICROSERVICE_NAME } from '@libs/common';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    AuthModule,
     ClientsModule.register([
       {
         name: MICROSERVICE_NAME.USER_SERVICE,
