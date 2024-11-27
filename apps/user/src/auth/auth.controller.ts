cd1af07052f638b7a0468c2fdadda1f8ee2710f5
Jin-devST <kpasd002@gmail.com>
2024-11-27 11:47:09 +0900

    feat. User 모듈 + 인증 모듈 병합

---
+++ apps/user/src/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Headers, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { KakaoGuard, LocalGuard } from './guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  // local 보안 로직은 직접 구현
+  @UseGuards(LocalGuard)
+  @Post('login')
+  async login(@Req() req) {
+    return await this.authService.loginExecutes(req.user);
+  }
+
+  @UseGuards(KakaoGuard)
+  @Post('login/kakao')
+  async kakaoLogin() {
+    // return this.authService.kakaoLogin();
+  }
+
+  @Post('/re-issue')
+  async reissueTokens(@Headers('authorization') authorization: string) {
+    return await this.authService.reissueTokensExecutes(authorization);
+  }
+
+  // @MessagePattern({cmd: 'login'})
+  // @EventPattern('login')
+  // async login(user: User) {
+  //   return await this.authService.loginExecutes(user);
+  // }
+}
--- apps/user/src/auth/auth.module.ts
+++ apps/user/src/auth/auth.module.ts
@@ -2,18 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { EncryptionModule } from '@libs/encryption';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [
-    EncryptionModule,
-    ClientsModule.register([
-      {
-        name: 'USER_SERVICE',
-        transport: Transport.TCP,
-      },
-    ]),
-  ],
+  imports: [EncryptionModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
