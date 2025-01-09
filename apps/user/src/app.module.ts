59f082be15520bcd76f688ad154357a7e2db3191
Jin-devST <kpasd002@gmail.com>
2025-01-09 21:57:33 +0900

    feat. bearer-token.middleware.ts 에서 유저 토큰 캐싱 기능 구현

--- apps/user/src/app.module.ts
+++ apps/user/src/app.module.ts
@@ -1,49 +1,55 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { Environment, ENVIRONMENT_KEYS, Joi } from '@libs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
+import {Module} from '@nestjs/common';
+import {UserModule} from './user/user.module';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import {Environment, ENVIRONMENT_KEYS, Joi} from '@libs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {AuthModule} from './auth/auth.module';
+import {CacheModule} from "@nestjs/cache-manager";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: './apps/user/.env',
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.valid(
-          Environment.DEVELOPMENT,
-          Environment.PRODUCTION,
-        ).required(),
-        DATABASE_URL: Joi.string().required(),
-        GRPC_USER_SERVICE_URL: Joi.string().required(),
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: './apps/user/.env',
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.valid(
+                    Environment.DEVELOPMENT,
+                    Environment.PRODUCTION,
+                ).required(),
+                DATABASE_URL: Joi.string().required(),
+                GRPC_USER_SERVICE_URL: Joi.string().required(),
 
-        KAKAO_CLIENT_ID: Joi.string().required(),
-        KAKAO_CLIENT_SECRET: Joi.string().required(),
-        KAKAO_REDIRECT_URI: Joi.string().required(),
+                KAKAO_CLIENT_ID: Joi.string().required(),
+                KAKAO_CLIENT_SECRET: Joi.string().required(),
+                KAKAO_REDIRECT_URI: Joi.string().required(),
 
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_DB: Joi.string().required(),
+                POSTGRES_USER: Joi.string().required(),
+                POSTGRES_PASSWORD: Joi.string().required(),
+                POSTGRES_DB: Joi.string().required(),
 
-        AUTH_ROUNDS: Joi.number().required(),
-        JWT_ACCESS_TOKEN_SECRET: Joi.string().required(),
-        JWT_REFRESH_TOKEN_SECRET: Joi.string().required(),
-      }),
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        url: configService.getOrThrow(ENVIRONMENT_KEYS.DATABASE_URL),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-    }),
-    UserModule,
-    AuthModule,
-  ],
-  controllers: [],
-  providers: [],
+                AUTH_ROUNDS: Joi.number().required(),
+                JWT_ACCESS_TOKEN_SECRET: Joi.string().required(),
+                JWT_REFRESH_TOKEN_SECRET: Joi.string().required(),
+            }),
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                url: configService.getOrThrow(ENVIRONMENT_KEYS.DATABASE_URL),
+                autoLoadEntities: true,
+                synchronize: true,
+            }),
+        }),
+        CacheModule.register({
+            ttl: 2000,
+            isGlobal: true,
+        }),
+        UserModule,
+        AuthModule,
+    ],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
--- libs/common/src/middlewares/bearer-token.middleware.ts
+++ libs/common/src/middlewares/bearer-token.middleware.ts
@@ -1,68 +1,76 @@
-import {
-  Inject,
-  Injectable,
-  NestMiddleware,
-  OnModuleInit,
-  UnauthorizedException,
-} from '@nestjs/common';
-import { NextFunction, Request, Response } from 'express';
-import { ClientGrpc } from '@nestjs/microservices';
-import { MICROSERVICE_NAME, UserMicroService } from '@libs/common';
-import { lastValueFrom } from 'rxjs';
+import {Inject, Injectable, NestMiddleware, OnModuleInit, UnauthorizedException,} from '@nestjs/common';
+import {NextFunction, Request, Response} from 'express';
+import {ClientGrpc} from '@nestjs/microservices';
+import {dayjs, MICROSERVICE_NAME, UserMicroService} from '@libs/common';
+import {lastValueFrom} from 'rxjs';
+import {Cache, CACHE_MANAGER} from "@nestjs/cache-manager";
 
 @Injectable()
 export class BearerTokenMiddleware implements NestMiddleware, OnModuleInit {
-  authService: UserMicroService.AuthServiceClient;
-  userService: UserMicroService.UserServiceClient;
+    authService: UserMicroService.AuthServiceClient;
+    userService: UserMicroService.UserServiceClient;
 
-  constructor(
-    @Inject(MICROSERVICE_NAME.USER_SERVICE)
-    private readonly userMicroService: ClientGrpc,
-  ) {}
-
-  onModuleInit() {
-    this.authService =
-      this.userMicroService.getService<UserMicroService.AuthServiceClient>(
-        UserMicroService.AUTH_SERVICE_NAME,
-      );
-    this.userService =
-      this.userMicroService.getService<UserMicroService.UserServiceClient>(
-        MICROSERVICE_NAME.USER_SERVICE,
-      );
-  }
-
-  async use(req: Request, res: Response, next: NextFunction) {
-    const authorization = req.headers.authorization;
-    try {
-      if (!authorization) {
-        throw new UnauthorizedException('토큰이 필요합니다.');
-      }
-      const payload = await lastValueFrom(
-        this.authService.parseBearerToken({ token: authorization }),
-      );
-
-      if (
-        [
-          UserMicroService.TokenType.ACCESS,
-          UserMicroService.TokenType.REFRESH,
-        ].includes(payload.type)
-      ) {
-        throw new UnauthorizedException('토큰의 타입이 올바르지 않습니다.');
-      }
-
-      // this.userMicroService.
-      const user = await lastValueFrom(
-        this.userService.getUser({ id: payload.sub }),
-      );
-
-      if (!user) {
-        throw new UnauthorizedException('사용자를 찾을 수 없습니다.');
-      }
-
-      req.user = payload;
-      next();
-    } catch (e) {
-      throw new UnauthorizedException('토큰이 올바르지 않습니다.');
+    constructor(
+        @Inject(MICROSERVICE_NAME.USER_SERVICE)
+        private readonly userMicroService: ClientGrpc,
+        @Inject(CACHE_MANAGER)
+        private readonly cacheManager: Cache
+    ) {
     }
-  }
+
+    onModuleInit() {
+        this.authService =
+            this.userMicroService.getService<UserMicroService.AuthServiceClient>(
+                UserMicroService.AUTH_SERVICE_NAME,
+            );
+        this.userService =
+            this.userMicroService.getService<UserMicroService.UserServiceClient>(
+                MICROSERVICE_NAME.USER_SERVICE,
+            );
+    }
+
+    async use(req: Request, res: Response, next: NextFunction) {
+        const authorization = req.headers.authorization;
+        try {
+            if (!authorization) {
+                throw new UnauthorizedException('토큰이 필요합니다.');
+            }
+            const payload = await lastValueFrom(
+                this.authService.parseBearerToken({token: authorization}),
+            );
+
+            if (
+                [
+                    UserMicroService.TokenType.ACCESS,
+                    UserMicroService.TokenType.REFRESH,
+                ].includes(payload.type)
+            ) {
+                throw new UnauthorizedException('토큰의 타입이 올바르지 않습니다.');
+            }
+            const cachedPayload = await this.cacheManager.get(`user:${payload.sub}`);
+            if(cachedPayload){
+              req.user = cachedPayload;
+              return next();
+            }
+            const user = await lastValueFrom(
+                this.userService.getUser({id: payload.sub}),
+            );
+
+            if (!user) {
+                throw new UnauthorizedException('사용자를 찾을 수 없습니다.');
+            }
+
+            const expireDate = dayjs(payload.exp).unix();
+            const now = dayjs().unix();
+            const diff = expireDate - now;
+            if (diff < 0) {
+                throw new UnauthorizedException('토큰이 만료되었습니다.');
+            }
+            await this.cacheManager.set(`user:${user.user.id}`, user, diff);
+            req.user = payload;
+            next();
+        } catch (e) {
+            throw new UnauthorizedException('토큰이 올바르지 않습니다.');
+        }
+    }
 }
